817934afb1d1fed6a34e5464c4f17adb8eda0217
Heitor Stael <heitorstael@live.com>
2020-10-19 19:03:53 -0300

    ajeitando variaveis ambiente

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `http://${process.env.REACT_APP_API_HOST}:${process.env.REACT_APP_API_PORT}`
+  baseURL: `http://${process.env.REACT_APP_BACKEND_HOST}:${process.env.REACT_APP_BACKEND_PORT}`
 });
 
 export default api;
